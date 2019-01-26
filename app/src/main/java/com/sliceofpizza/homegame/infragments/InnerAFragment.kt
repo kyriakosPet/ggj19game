b257350e5ce52c672e2b22d701d3c838efcf808f
AcidDr3amer <nmandrelas@gmail.com>
2019-01-26 20:03:07 +0200

    -- swipping added

--- app/src/main/java/com/sliceofpizza/homegame/infragments/InnerAFragment.kt
+++ app/src/main/java/com/sliceofpizza/homegame/infragments/InnerAFragment.kt
@@ -2,13 +2,14 @@ package com.sliceofpizza.homegame.infragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import com.sliceofpizza.homegame.R
+import com.sliceofpizza.homegame.MainActivity
+import com.sliceofpizza.homegame.activities.InActivity
+import kotlinx.android.synthetic.main.fragment_inner_a.*
 
 
-class InnerAFragment : Fragment() {
+class InnerAFragment : Fragment()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +18,27 @@ class InnerAFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
+        this.view!!.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                when (event?.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        lever.animate().translationYBy(-100f).setDuration(400).withEndAction {
+                            (activity as InActivity).myRef!!.child("levelUp").setValue(true)
+                        }
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        lever.animate().translationYBy(-100f).setDuration(400).withEndAction {
+                            (activity as InActivity).myRef!!.child("levelUp").setValue(false)
+                        }
+                    }
+                }
+
+                return v?.onTouchEvent(event) ?: true
+            }
+        })
+
         return inflater.inflate(R.layout.fragment_inner_a, container, false)
+
     }
 }
