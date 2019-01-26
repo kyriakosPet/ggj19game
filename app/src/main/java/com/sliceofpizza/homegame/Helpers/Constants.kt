4e7692ace03823fa05d0b1cce8b18926365c8b02
kyriakos petrolias <kyriakos.pet@gmail.com>
2019-01-26 20:03:44 +0200

    spwan alliens

--- app/src/main/java/com/sliceofpizza/homegame/Helpers/Constants.kt
+++ app/src/main/java/com/sliceofpizza/homegame/Helpers/Constants.kt
@@ -22,5 +22,6 @@ class Constants {
         const val wasteFillAmount: Double = 2.0
         var hasWaste = false
         const val wasteRepeat: Long = 1250
+        const val alienspawnRepeat: Long = 40000
     }
 }
