6aad812889cfb83f221a91f9796026b68d3f8d28
kyriakos petrolias <kyriakos.pet@gmail.com>
2019-01-26 18:42:55 +0200

    fullscreen

--- app/src/main/java/com/sliceofpizza/homegame/outfragments/CFragment.kt
+++ app/src/main/java/com/sliceofpizza/homegame/outfragments/CFragment.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.betheres.krsreporting.AppObject
 import com.betheres.krsreporting.com.sliceofpizza.homegame.Helpers.AlienHelper
 import com.betheres.krsreporting.com.sliceofpizza.homegame.Helpers.Coordinate
 import com.google.firebase.database.DataSnapshot
@@ -19,8 +20,10 @@ import java.util.ArrayList
 
 class CFragment : Fragment() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -53,8 +56,8 @@ var waypointNumer=0
     fun spawnAlien(){
         waypoints=AlienHelper.getAliensMovement()
 
-        alien.x=waypoints[0].x.toFloat()
-        alien.y=waypoints[0].y.toFloat()
+        alien.x=waypoints[0].x.toScreenWidth
+        alien.y=waypoints[0].y.toScreenHeight
         nextWayPoint(waypointNumer)
 
     }
@@ -64,9 +67,8 @@ var waypointNumer=0
             alienHitMe()
             return
         }
-        Log.d("eeee","pos x "+waypoints[waypointNumer].x + "pos y "+waypoints[waypointNumer].y)
 
-        alien.animate().x(waypoints[waypointNumer].x.toFloat()).y(waypoints[waypointNumer].y.toFloat()).setDuration(1000).withEndAction {
+        alien.animate().x(waypoints[waypointNumer].x.toScreenWidth).y(waypoints[waypointNumer].y.toScreenHeight).setDuration(2000).withEndAction {
             this.waypointNumer++
         nextWayPoint(this.waypointNumer)}
     }
@@ -81,3 +83,12 @@ var waypointNumer=0
 
 
 }
+
+private val Int.toScreenWidth: Float
+    get() {
+        return this/100.toFloat()*AppObject.getScreenDimensions().x
+    }
+private val Int.toScreenHeight: Float
+    get() {
+        return this/100.toFloat()*AppObject.getScreenDimensions().y
+    }
