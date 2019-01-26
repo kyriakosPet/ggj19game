1358fcb522a59e0949d8dbbfbeaa4f6327c995e0
AcidDr3amer <nmandrelas@gmail.com>
2019-01-26 17:47:27 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	.idea/modules.xml
    #	app/build.gradle

--- app/src/main/java/com/sliceofpizza/homegame/Helpers/Constants.kt
+++ app/src/main/java/com/sliceofpizza/homegame/Helpers/Constants.kt
@@ -5,5 +5,10 @@ class Constants {
         val alienLife = 8
         val alienSecondHalf = 4
         val alienDamage = 10
+
+        val width1 = 33
+        val width2 = 66
+        val width3 = 100
+
     }
 }
