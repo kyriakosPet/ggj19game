1c8968033041a0ca2169726c6c28b749de44fc4e
nmandrelas@gmail.com <nmandrelas@gmail.com>
2019-01-26 14:37:18 +0200

    - added cycle view pager

--- app/src/main/java/com/sliceofpizza/homegame/activities/InActivity.kt
+++ app/src/main/java/com/sliceofpizza/homegame/activities/InActivity.kt
@@ -3,11 +3,18 @@ package com.sliceofpizza.homegame.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.sliceofpizza.homegame.R
+import kotlinx.android.synthetic.main.activity_in.*
 
 class InActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_in)
+
+        createPager()
+    }
+
+    private fun createPager() {
+        
     }
 }
