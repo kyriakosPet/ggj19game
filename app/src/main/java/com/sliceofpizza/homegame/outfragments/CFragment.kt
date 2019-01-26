06bdf19d43320a78b058f210957d1ea3ae7a93f8
kyriakos petrolias <kyriakos.pet@gmail.com>
2019-01-26 15:22:10 +0200

    NEW PAGER

--- app/src/main/java/com/sliceofpizza/homegame/outfragments/CFragment.kt
+++ app/src/main/java/com/sliceofpizza/homegame/outfragments/CFragment.kt
@@ -18,7 +18,7 @@ class CFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_a, container, false)
+        return inflater.inflate(R.layout.fragment_c, container, false)
     }
 
 
