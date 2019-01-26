8dd8401bc482c417b561f534640fb7f480aad435
kyriakos petrolias <kyriakos.pet@gmail.com>
2019-01-26 13:54:25 +0200

    basic setup

---
+++ app/src/main/java/com/sliceofpizza/homegame/activities/InActivity.kt
@@ -0,0 +1,13 @@
+package com.sliceofpizza.homegame.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.sliceofpizza.homegame.R
+
+class InActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_in)
+    }
+}
---
+++ app/src/main/java/com/sliceofpizza/homegame/outfragments/AFragment.kt
@@ -0,0 +1,25 @@
+package com.sliceofpizza.homegame.outfragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.sliceofpizza.homegame.R
+
+class AFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+
+
+}
