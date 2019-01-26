6d2809690b97bd4acc688d752bb81e1e62566c78
Kyriakos Petrolias <kyriakos.pet@threenitas.com>
2019-01-26 02:26:04 +0200

    firebase database

---
+++ app/src/main/java/AppObject.kt
@@ -0,0 +1,25 @@
+package com.betheres.krsreporting
+
+import android.app.Application
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+/**
+ * Created by KyriakosPetrolias on 12/22/2017.
+ */
+class AppObject : Application() {
+
+
+    companion object {
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+
+}
--- app/src/main/java/com/sliceofpizza/homegame/MainActivity.kt
+++ app/src/main/java/com/sliceofpizza/homegame/MainActivity.kt
@@ -2,11 +2,52 @@ package com.sliceofpizza.homegame
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    var database: FirebaseDatabase? = null
+    var myRef: DatabaseReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        setupFirebaseDatabase()
+
+        btn.setOnClickListener {
+            changeValue()
+        }
     }
+
+
+    private fun changeValue() {
+        myRef?.child("message")?.setValue("hello world " + System.currentTimeMillis())
+    }
+
+
+    private fun setupFirebaseDatabase() {
+        database = FirebaseDatabase.getInstance()
+        myRef = database!!.getReference("gamestatus")
+
+
+        myRef!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                //val value = dataSnapshot.getValue(String::class.java)
+                valuetxt.text = dataSnapshot.child("message").value.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("eeeee", "Failed to read value.", error.toException())
+            }
+        })
+    }
+
 }
