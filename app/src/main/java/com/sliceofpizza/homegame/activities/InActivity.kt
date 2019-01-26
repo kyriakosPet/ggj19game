d56949be725c3e72c9349e0a6c44418ca6921938
nmandrelas@gmail.com <nmandrelas@gmail.com>
2019-01-26 15:24:40 +0200

    - added adapter that crashes

--- app/src/main/java/com/sliceofpizza/homegame/activities/InActivity.kt
+++ app/src/main/java/com/sliceofpizza/homegame/activities/InActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import android.view.animation.LinearInterpolator
 import com.sliceofpizza.homegame.R
 import com.sliceofpizza.homegame.infragments.InnerAFragment
 import com.sliceofpizza.homegame.infragments.InnerBFragment
@@ -24,21 +25,57 @@ class InActivity : AppCompatActivity() {
     private fun createPager() {
         val adapter = ViewPagerAdapter(supportFragmentManager, 4)
         pager.adapter = adapter
+//        pager.scrollDuration = 500;
+//        pager.interpolator = LinearInterpolator();
+//        pager.isMediumScaled = false;
+//        pager.maxPageScale = 1f
+//        pager.minPageScale = 1f
+//        pager.centerPageScaleOffset = 0f
+//        pager.minPageScaleOffset = 0f
     }
 
     private class ViewPagerAdapter(fm: FragmentManager, val pages: Int) : FragmentPagerAdapter(fm) {
 
-        private val fragments: Array<Fragment> = Array(4) {
-            when(it){
-                0 -> InnerAFragment()
-                1 -> InnerBFragment()
-                2 -> InnerCFragment()
-                else -> InnerDFragment()
-            }
-        }
+        private var a: Fragment? = null
+        private var b: Fragment? = null
+        private var c: Fragment? = null
+        private var d: Fragment? = null
 
         override fun getItem(position: Int): Fragment? {
-            return fragments[position]
+            return when(position){
+                0 -> {
+                    if (a == null) {
+                        a = InnerAFragment()
+                        a
+                    }else {
+                        a
+                    }
+                }
+                1 -> {
+                    if (b == null) {
+                        b = InnerBFragment()
+                        b
+                    }else {
+                        b
+                    }
+                }
+                2 -> {
+                    if (c == null) {
+                        c = InnerCFragment()
+                        c
+                    }else {
+                        c
+                    }
+                }
+                else -> {
+                    if (d == null) {
+                        d = InnerDFragment()
+                        d
+                    }else {
+                        d
+                    }
+                }
+            }
         }
 
         override fun getCount(): Int {
