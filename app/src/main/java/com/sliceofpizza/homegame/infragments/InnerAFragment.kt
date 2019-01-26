7a67e79bff7d2bf7dfb1bc7fee8e76530c9a57d9
AcidDr3amer <nmandrelas@gmail.com>
2019-01-26 20:11:27 +0200

    -- swipping added

--- app/src/main/java/com/sliceofpizza/homegame/infragments/InnerAFragment.kt
+++ app/src/main/java/com/sliceofpizza/homegame/infragments/InnerAFragment.kt
@@ -11,6 +11,8 @@ import kotlinx.android.synthetic.main.fragment_inner_a.*
 
 class InnerAFragment : Fragment()  {
 
+    var isUp = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -19,17 +21,29 @@ class InnerAFragment : Fragment()  {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
 
+        return inflater.inflate(R.layout.fragment_inner_a, container, false)
+
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         this.view!!.setOnTouchListener(object : View.OnTouchListener {
             override fun onTouch(v: View?, event: MotionEvent?): Boolean {
                 when (event?.action) {
                     MotionEvent.ACTION_DOWN -> {
-                        lever.animate().translationYBy(-100f).setDuration(400).withEndAction {
-                            (activity as InActivity).myRef!!.child("levelUp").setValue(true)
+                        if ( !isUp ) {
+                            isUp = true
+                            lever.animate().translationYBy(-100f).setDuration(400).withEndAction {
+                                (activity as InActivity).myRef!!.child("levelUp").setValue(true)
+                            }
                         }
                     }
                     MotionEvent.ACTION_UP -> {
-                        lever.animate().translationYBy(-100f).setDuration(400).withEndAction {
-                            (activity as InActivity).myRef!!.child("levelUp").setValue(false)
+                        if (isUp) {
+                            isUp = false
+                            lever.animate().translationYBy(100f).setDuration(400).withEndAction {
+                                (activity as InActivity).myRef!!.child("levelUp").setValue(false)
+                            }
                         }
                     }
                 }
@@ -37,8 +51,5 @@ class InnerAFragment : Fragment()  {
                 return v?.onTouchEvent(event) ?: true
             }
         })
-
-        return inflater.inflate(R.layout.fragment_inner_a, container, false)
-
     }
 }
